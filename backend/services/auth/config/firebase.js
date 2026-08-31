e6b3122b40832b670637935ed8425266c87c76cf
Saurab Gautam <missionprojects250@gmail.com>
2026-08-31 12:06:42 +0545

    03-setupping firebase for authentiucation

---
+++ backend/services/auth/config/firebase.js
@@ -0,0 +1,6 @@
+import { cert, initializeApp } from "firebase-admin/app";
+import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
+
+export const app = initializeApp({
+  credential: cert(serviceAccount)
+})
---
+++ backend/services/auth/models/user.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  firebaseUid:{
+    type: String,
+    required: true,
+    unique: true
+  },
+  name:{
+    type: String,
+    required: true
+  },
+  email:{
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true
+  },
+  avatar:{
+    type: String,
+    default: ""
+  }
+  }, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+export default User;
