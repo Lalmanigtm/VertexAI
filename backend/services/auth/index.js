ee5abbdd0bd3157aae199ae009a01fd8b577953c
Saurab Gautam <missionprojects250@gmail.com>
2026-08-31 16:34:57 +0545

    04-setupping full controller,routes

--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -1,14 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import router from "./routes/auth.routes.js";
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 const PORT = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.json({ message: "Auth service is running" });
-})
+app.use("/auth", router);
 
 app.listen(PORT, () => {
   connectDB();
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 // import React from 'react'
+import {auth, googleProvider} from './firebase.js'
+import {signInWithPopup} from 'firebase/auth'
 
 const App = () => {
+  const handleLogin = async () => {
+    const data = await signInWithPopup(auth, googleProvider);
+    console.log(data);
+  }
+
   return (
     <div>
-      <button>Hello Maan CLICK ME </button>
+      <button onClick={handleLogin}>Hello Maan CLICK ME </button>
 
     </div>
   )
