c4e12f4ce1e6e10f7c16af87d2c707488de88056
Saurab Gautam <missionprojects250@gmail.com>
2026-09-02 22:37:05 +0545

    push better code but not work

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -2,5 +2,5 @@ import { cert, initializeApp } from "firebase-admin/app";
 import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
 
 export const app = initializeApp({
-  credential: cert(serviceAccount)
-})
+  credential: cert(serviceAccount),
+});
