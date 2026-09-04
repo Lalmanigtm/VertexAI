0b1068584c1cb508402567c215578422c626a8d6
Saurab Gautam <missionprojects250@gmail.com>
2026-09-04 23:11:20 +0545

    further more with models

--- backend/services/auth/models/user.model.js
+++ backend/services/auth/models/user.model.js
@@ -1,26 +1,41 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  firebaseUid:{
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    firebaseUid: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+      trim: true,
+    },
+
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true,
+    },
+
+    avatar: {
+      type: String,
+      default: "",
+      trim: true,
+    },
   },
-  name:{
-    type: String,
-    required: true
+  {
+    timestamps: true,
   },
-  email:{
-    type: String,
-    required: true,
-    trim: true,
-    lowercase: true
-  },
-  avatar:{
-    type: String,
-    default: ""
-  }
-  }, { timestamps: true });
+);
 
 const User = mongoose.model("User", userSchema);
+
 export default User;
